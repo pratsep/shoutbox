ea3391274dca40910c77c1832c95c7d038f35844
Priit Rätsep <pratsep@itcollege.ee>
2017-03-06 20:01:46 +0200

    proov

--- index.php
+++ index.php
@@ -25,44 +25,52 @@
       <textarea name="comment" form="insert_form" placeholder="Sisesta tekst" onkeydown="pressed(event)" required></textarea>
       <input id="submit_button" type="submit" value="Postita"/>
 		</form>
-    <?php
+   </div>
+    <div class="comments">
+		<?php
       $sql = "SELECT username, comment, time FROM pratsep_shoutbox order by time desc";
       $result = mysqli_query($conn, $sql);
       $pages = ceil(mysqli_num_rows($result))/10;
       //tere
+      echo '<div class="pages">';
+      if ($pages>1 && isset($_GET['page']) && $_GET['page']>1) {
+        $pagenr1 = $_GET['page'] - 1;
+        $addr1 = "window.location.href='?page=$pagenr1'";
+        echo '<button onclick="'.$addr1.'">Previous</button>';
+      }
       if ($pages > 1) {
-        echo '<div class="pages">';
-        for ($i=1; $i < $pages + 1; $i++) {
+        for ($i=$_GET['page']; $i < $_GET['page'] + 10; $i++) {
           $addr = "window.location.href='?page=$i'";
           echo '<button onclick="'.$addr.'">'.$i.'</button>';
         }
-        echo '</div>';
       }
-    ?>
-   </div>
-    <div class="comments">
-		<?php
-    if (isset($_GET['page']) && $_GET['page']>1) {
-      $ofset = (($_GET['page'])*10) - 10;
-    }
-    else{
-      $ofset = 0;
-    }
-		$sql = "SELECT username, comment, time FROM pratsep_shoutbox order by time desc LIMIT 10 OFFSET $ofset";
-		$result = mysqli_query($conn, $sql);
-		if (mysqli_num_rows($result) > 0) {
-      while($row = $result->fetch_assoc()) {
-  			echo '<div class ="comment">';
-  			echo '<h1>'.$row["username"].'</h1>';
-  			echo '<h3>'.$row["time"].'</h3>';
-  			echo '<p>'.$row["comment"].'</p>';
-  			//echo "User: " . $row["username"]. " - Comment: " . $row["comment"]. " " . $row["time"]. "<br>";
-  			echo '</div>';
+      if ($pages>1 && isset($_GET['page']) && $_GET['page']<$pages) {
+        $pagenr2 = $_GET['page'] + 1;
+        $addr2 = "window.location.href='?page=$pagenr2'";
+        echo '<button onclick="'.$addr2.'">Next</button>';
       }
-	  }
-		else {
-			echo "<h2>MEIE KARJUMISALA ON VEEEEL TÜHI!</h2>";
-		}
+      echo '</div>';
+      if (isset($_GET['page']) && $_GET['page']>1) {
+        $ofset = (($_GET['page'])*10) - 10;
+      }
+      else{
+        $ofset = 0;
+      }
+  		$sql = "SELECT username, comment, time FROM pratsep_shoutbox order by time desc LIMIT 10 OFFSET $ofset";
+  		$result = mysqli_query($conn, $sql);
+  		if (mysqli_num_rows($result) > 0) {
+        while($row = $result->fetch_assoc()) {
+    			echo '<div class ="comment">';
+    			echo '<h1>'.$row["username"].'</h1>';
+    			echo '<h3>'.$row["time"].'</h3>';
+    			echo '<p>'.$row["comment"].'</p>';
+    			//echo "User: " . $row["username"]. " - Comment: " . $row["comment"]. " " . $row["time"]. "<br>";
+    			echo '</div>';
+        }
+  	  }
+  		else {
+  			echo "<h2>MEIE KARJUMISALA ON VEEEEL TÜHI!</h2>";
+  		}
 		?>
     </div>
     <?php
